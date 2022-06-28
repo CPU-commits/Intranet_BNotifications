2616a7adff3b64c271e11a62423e7cdf2f8d2fc7
Felipe Cárdenas <felipecardenas301@gmail.com>
2022-06-28 16:30:01 -0400

    [update]  Add NATS and cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule)
+    // NATS Microservice
+    app.connectMicroservice<MicroserviceOptions>({
+        transport: Transport.NATS,
+        options: {
+            servers: ['nats://nats:4222'],
+        },
+    })
+    app.enableCors({
+        origin: '*',
+        methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    })
+    await app.startAllMicroservices()
+    await app.listen(7000)
 }
-bootstrap();
+bootstrap()
